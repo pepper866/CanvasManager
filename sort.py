f7e532f7ca5833b7edc947b9c0b6c37b022ada25
Tom Rohrbach <31772006+pepper866@users.noreply.github.com>
2023-04-02 22:00:44 -0400

    alphabetical sort

--- sort.py
+++ sort.py
@@ -17,3 +17,32 @@ API_KEY = "6723~A2KTPfsPob1ZYugZg3xsrJWaA94bathpwkDemhIyUcZNNGMiTekg6CNtoiFAVtCW
 canvas = Canvas(API_URL, API_KEY)
 # Grab course 123456
 course = canvas.get_course(COURSE_ID)
+
+#get modules
+modules = course.get_modules()
+
+#compile list of module names to sort
+moduleNames = []
+moduleTable = {}
+for module in modules:
+    moduleNames.append(module.name)
+    
+    #create a map from name to id to easily grab later
+    moduleTable[module.name] = module.id
+    
+#sort module names
+moduleNames.sort()
+print(moduleNames)
+print(moduleTable)
+    
+#go through all names in order, then edit their position
+index = 1
+for name in moduleNames:
+    print(name)
+    print(moduleTable[name])
+    print(index)
+    module = course.get_module(moduleTable[name])
+    print(module.position)
+    module.edit(module = {'position': index})
+                
+    index+=1
