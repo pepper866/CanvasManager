d9b381bce1178f9d29ebdf94040ea04d121c6268
Tom Rohrbach <31772006+pepper866@users.noreply.github.com>
2023-04-26 21:42:42 -0400

    can clear stuff now

--- ClearTab.py
+++ ClearTab.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from ttkwidgets import CheckboxTreeview
 from CanvasConnection import getAssignments
 from CanvasConnection import deleteAssignments
+from CanvasConnection import getModules
+from CanvasConnection import deleteModules
+from CanvasConnection import getFiles
+from CanvasConnection import deleteFiles
 
 def makeClearTab(root):
     # Clear Tab (tab3)
@@ -19,13 +23,29 @@ def makeClearTab(root):
     for o in options:
         tree.insert("", "end", o, text=o)
 
+    # method to select all options
     def select_all():
         for o in options:
             if o not in tree.get_checked():
                 tree.change_state(o, "checked")
 
+    # method to clear canvas page of whichever options were chosen
     def clear_selected():
-        return
+        
+        # get whichever options were checked
+        selected = tree.get_checked()
+        
+        # delete selected options from canvas
+        if "Modules" in selected:
+            modules = getModules()
+            deleteModules(modules)
+        if "Assignments" in selected:
+            assignments = getAssignments()
+            deleteAssignments(assignments)
+        if "Files" in selected:
+            files = getFiles()
+            deleteFiles(files)
+            
     
     selectAll = tk.Button(root, text="Select All Options", command=select_all)
     selectAll.pack(side="top", pady = 10)
